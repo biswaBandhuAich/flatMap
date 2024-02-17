2727c1bba7664aa2d276564cbaa135edf7c3587f
Biswa Bandhu Aich <49384822+biswaBandhuAich@users.noreply.github.com>
2024-02-17 09:37:05 +0530

    deploy commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3BAMIxMlIe-IQVP0RRftgIdmyxwyN4Xs",
+    authDomain: "flatmap1308.firebaseapp.com",
+    databaseURL: "https://flatmap1308-default-rtdb.firebaseio.com",
+    projectId: "flatmap1308",
+    storageBucket: "flatmap1308.appspot.com",
+    messagingSenderId: "364754727927",
+    appId: "1:364754727927:web:ffc5bc94f7ea14b91ab4f2"
+  }
 };
